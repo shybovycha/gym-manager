90498f83355c9c56dd00b0f9c1c12c357fd83639
Artem Shoobovych <shybovycha@gmail.com>
2015-03-20 16:08:12 +0100

    Created data model. Generated auth views

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.paths += File.join(Rails.root, 'bower_components')
+Rails.application.config.assets.paths << File.join(Rails.root, 'bower_components')
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  resources :clubs
+
+  resources :workouts
+
+  resources :members
+
+  devise_for :users
+  resources :rails
+
+  resources :owners
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
